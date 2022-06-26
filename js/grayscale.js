f22210c3d07b40f629255b941e99d8dec9b95aea
Jamie Douglass <jamie@douglass.com>
2022-06-25 20:50:51 -0700

    Responsive menu fix

--- js/grayscale.js
+++ js/grayscale.js
@@ -26,7 +26,7 @@ $(function() {
 
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $('.navbar-toggle:visible').click();
+    $('.:visible').click();
 });
 
 // Google Maps Scripts
